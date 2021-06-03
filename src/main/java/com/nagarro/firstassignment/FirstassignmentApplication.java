9ddcddc0f7cbb7fab736eeff0e53d01ccdbea0e0
ankitkumar03 <ankit.kumar03@nagarro.com>
2021-06-03 14:48:33 +0530

    hi

---
+++ src/main/java/com/nagarro/firstassignment/FirstassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.nagarro.firstassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstassignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagarro/firstassignment/FirstassignmentApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nagarro.firstassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	
+	}
+
+}
